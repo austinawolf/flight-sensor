93b0bb620875c84136f835df10ab71cbcc39f9e7
Austin Wolf <austin.wolf@novoengineering.com>
2022-06-12 19:13:54 -0700

    fix compilation issue

--- nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp_btn_ble.c
+++ nRF5_SDK_17.1.0_ddde560/components/libraries/bsp/bsp_btn_ble.c
@@ -163,20 +163,8 @@ static uint32_t advertising_buttons_configure()
  * @param[out] p_startup_event  Where to put the extracted BSP event.
  */
 static void startup_event_extract(bsp_event_t * p_startup_event)
-{
-    // React to button states
-    if (bsp_button_is_pressed(BTN_ID_WAKEUP_BOND_DELETE))
-    {
-        *p_startup_event = BSP_EVENT_CLEAR_BONDING_DATA;
-    }
-    else if (bsp_button_is_pressed(BTN_ID_WAKEUP))
-    {
-        *p_startup_event = BSP_EVENT_WAKEUP;
-    }
-    else
-    {
-        *p_startup_event = BSP_EVENT_NOTHING;
-    }
+{   
+    *p_startup_event = BSP_EVENT_NOTHING;
 }
 
 
